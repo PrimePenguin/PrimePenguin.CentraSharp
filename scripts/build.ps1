afb60a9f60e887bd6d98da15e25add911b13e1ce
SouravThakur <souravthakur1992@gmail.com>
2019-04-19 03:05:35 +0530

    Update build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,6 +1,6 @@
 dotnet restore;
 dotnet build -c Release;
-dotnet pack --no-build -c Release CentraSharp/CentraSharp.csproj;
+dotnet pack --no-build -c Release PrimePenguin.CentraSharp/CentraSharp.csproj;
 
 $nupkg = (Get-ChildItem CentraSharp/bin/Release/*.nupkg)[0];
 
