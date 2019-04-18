b4b93f3d972cc19d2820266919de6c9ecfeb2b97
SouravThakur <souravthakur1992@gmail.com>
2019-04-19 02:41:25 +0530

    Create build.ps1

---
+++ CentraSharp/scripts/build.ps1
@@ -0,0 +1,8 @@
+dotnet restore;
+dotnet build -c Release;
+dotnet pack --no-build -c Release CentraSharp/CentraSharp.csproj;
+
+$nupkg = (Get-ChildItem CentraSharp/bin/Release/*.nupkg)[0];
+
+# Push the nuget package to AppVeyor's artifact list.
+Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "PrimePenguin.CentraSharp.nupkg";
