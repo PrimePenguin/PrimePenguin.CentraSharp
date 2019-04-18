3c184a974614fb65d3b500525ec8297374b507d6
SouravThakur <souravthakur1992@gmail.com>
2019-04-19 03:35:31 +0530

    update

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -2,7 +2,7 @@ dotnet restore;
 dotnet build -c Release;
 dotnet pack --no-build -c Release CentraSharp/PrimePenguin.CentraSharp.csproj;
 
-$nupkg = (Get-ChildItem primepenguin-centrasharp/CentraSharp/bin/Release/*.nupkg)[0];
+$nupkg = (Get-ChildItem CentraSharp/bin/Release/*.nupkg)[0];
 
 # Push the nuget package to AppVeyor's artifact list.
 Push-AppveyorArtifact $nupkg.FullName -FileName $nupkg.Name -DeploymentName "PrimePenguin.CentraSharp.nupkg";
