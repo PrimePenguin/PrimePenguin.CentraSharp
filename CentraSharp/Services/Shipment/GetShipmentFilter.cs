696f59d96324101f346b38b218c565dc116bbc71
Ajay Kumar <ajay@primepenguin.com>
2022-06-02 12:12:10 +0530

    updated GetShipmentFilter

--- CentraSharp/Services/Shipment/GetShipmentFilter.cs
+++ CentraSharp/Services/Shipment/GetShipmentFilter.cs
@@ -9,12 +9,36 @@ namespace PrimePenguin.CentraSharp.Services.Shipment
         /// Limit amount of orders returned.
         /// </summary>
         [JsonProperty("limit")]
-        public int Limit { get; set; }
+        public int? Limit { get; set; }
+
+        /// <summary>
+        /// Offset how far in to start returning orders.
+        /// </summary>
+        [JsonProperty("offset")]
+        public int? Offset { get; set; }
 
         /// <summary>
         /// TReturn a specific order.
         /// </summary>
         [JsonProperty("order")]
-        public int Order { get; set; }
+        public int? Order { get; set; }
+
+        /// <summary>
+        /// Return orders for specified customer id.
+        /// </summary>
+        [JsonProperty("customer_id")]
+        public int? CustomerId { get; set; }
+
+        /// <summary>
+        /// Return order newer than date YYYY-mm-dd HH:MM:SS
+        /// </summary>
+        [JsonProperty("newer_than")]
+        public string NewerThan { get; set; }
+
+        /// <summary>
+        /// Return order newer than date YYYY-mm-dd HH:MM:SS
+        /// </summary>
+        [JsonProperty("older_than")]
+        public string OlderThan { get; set; }
     }
 }
