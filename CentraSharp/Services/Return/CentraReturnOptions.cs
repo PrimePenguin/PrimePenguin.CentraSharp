3b1b20a7ebaa855726a2271e2aa7471105f627c3
Ajay Kumar <ajay@primepenguin.com>
2022-11-08 17:59:52 +0530

    added additional return parameters

--- CentraSharp/Services/Return/CentraReturnOptions.cs
+++ CentraSharp/Services/Return/CentraReturnOptions.cs
@@ -15,13 +15,37 @@ namespace PrimePenguin.CentraSharp.Services.Return
         /// Insert returned items into stock. If not provided Default stock action on return setting on the store will be used.
         /// </summary>
         [JsonProperty("returnStock")]
-        public bool ReturnStock { get; set; }
+        public bool? ReturnStock { get; set; }
+
+        /// <summary>
+        /// Default false. Set to true to use Handling cost from Shipment. You cannot provide handlingCost at the same time this option is used.
+        /// </summary>
+        [JsonProperty("handlingCostFromShipment")]
+        public bool? HandlingCostFromShipment { get; set; }
+
+        /// <summary>
+        /// Default false. Set to true to use Shipping cost from Shipment. You cannot provide shippingCost at the same time this option is used.
+        /// </summary>
+        [JsonProperty("shippingCostFromShipment")]
+        public bool? ShippingCostFromShipment { get; set; }
 
         /// <summary>
         /// Shipping cost.
         /// </summary>
         [JsonProperty("shippingCost")]
         public double? ShippingCost { get; set; }
+
+        /// <summary>
+        /// Return cost. If the return itself should have a cost to it. Providing a value here will deduct the amount of money to refund.
+        /// </summary>
+        [JsonProperty("returnCost")]
+        public double? ReturnCost { get; set; }
+
+        /// <summary>
+        /// Handling cost.
+        /// </summary>
+        [JsonProperty("handlingCost")]
+        public double? HandlingCost { get; set; }
 
         /// <summary>
         /// Additional comment for return, for reference.
